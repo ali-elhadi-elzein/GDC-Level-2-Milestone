fa979a118de1485bba24f0e9f65c2079bb28a9eb
Ali Elhadi Elzein <alielhadielzein@hotmail.com>
2024-07-13 16:12:24 +0300

    solved the milestone challenge

--- tasks.py
+++ tasks.py
@@ -7,10 +7,10 @@ try:
     command = None
     arguments = None
     if len(cli_args) == 0:
-        raise Exception("Arguments not supplied")
+        pass
     elif len(cli_args) == 1:
         command = cli_args[0]
-    if len(cli_args) > 1:
+    elif len(cli_args) > 1:
         command = cli_args[0]
         arguments = cli_args[1:]
     # Run the Task Command Class with the arguments supplied
